e972482cb8b2ff3c9e8e7a4d106b1b3c219b72a9
Allan <allan@datadecision.ai>
2023-06-01 02:01:13 +0800

    project commit

---
+++ setup.py
@@ -0,0 +1,71 @@
+import pathlib
+
+import pkg_resources
+from setuptools import setup, find_packages
+
+# with open("README.md", "r",encoding='utf-8-sig') as fh:
+#     long_description = fh.read()
+
+
+
+NAME = "prompt_is_all_you_need"
+DIR = '.'
+
+PACKAGES = find_packages(exclude= ["whisper",".idea","audio","generate_interviews"])
+print(PACKAGES)
+
+
+
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+
+
+
+setup(name=NAME,
+      version='0.0.1',
+      description='Prompt is all you need',
+      # long_description=long_description,
+      # long_description_content_type="text/markdown",
+      long_description=open("README.md", encoding="utf-8").read(),
+      long_description_content_type="text/markdown",
+      author='Allan Yiin',
+      author_email='allanyiin.ai@gmail.com',
+      download_url='https://test.pypi.org/project/prompt_is_all_you_need',
+      license='MIT',
+      install_requires=install_requires,
+      extras_require={
+          'tests': ['pytest',
+                    'pytest-pep8',
+                    'pytest-xdist',
+                    'pytest-cov',
+                    'requests',
+                    'markdown'],
+      },
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      python_requires='>=3.7',
+      keywords=['chatgpt', 'gpt4'],
+      packages= find_packages(exclude= ["whisper",".idea","audio","generate_interviews"]),
+      include_package_data=True,
+      scripts=[],
+
+      )
+
