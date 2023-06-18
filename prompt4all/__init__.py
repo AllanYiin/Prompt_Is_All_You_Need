8a4a22313e5492d19dab89486fa28abbef577567
Allan <allan@datadecision.ai>
2023-06-18 23:38:40 +0800

    update project structure

---
+++ prompt4all/__init__.py
@@ -0,0 +1,32 @@
+"""trident api"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import sys
+import os
+from importlib import reload
+from sys import stderr
+
+defaultencoding = 'utf-8'
+if sys.getdefaultencoding() != defaultencoding:
+    reload(sys)
+    sys.setdefaultencoding(defaultencoding)
+
+PACKAGE_ROOT = os.path.dirname(__file__)
+PROJECT_ROOT = os.path.dirname(PACKAGE_ROOT)
+
+__version__ = '0.0.1'
+stderr.write('prompt4all {0}\n'.format(__version__))
+
+from . import api
+from . import utils
+#from . import gradio_chatbot_patch
+#from . import theme
+import threading
+import random
+import glob
+from tqdm import tqdm
+import numpy as np
+
+
+
