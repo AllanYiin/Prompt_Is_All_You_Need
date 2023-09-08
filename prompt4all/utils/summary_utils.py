bdb281e6ae62f956128cb4fc3fea4a3e9a470f0b
Martin <martin.ku@skysource.com.tw>
2023-09-09 03:12:43 +0800

    Feat: Gen a num list from bullet list for summary

--- prompt4all/utils/summary_utils.py
+++ prompt4all/utils/summary_utils.py
@@ -1,3 +1,5 @@
+import os
+import re
 import builtins
 import markdown
 import copy
@@ -81,19 +83,94 @@ def get_rolling_summary_results(answer):
         return [t for t in content_dict[keys[item_check.index(max_items)]]if len(t.replace('\"\"\"',''))>0 and is_numbered_list_member(t)]
 
 
+def convert_bullet_to_number_list(content, linesep=os.linesep):
+    """Replace bullet point list with number list, for example
+
+    Sample input:
+    - 第03章新商業智慧平台安裝與設定
+      - 安裝SSRS 2012的前置需求
+        - 版本限制
+          - 標準版（Standard Edition）
+            - 提供報表設計、管理和部署功能
+            - 不支援進階功能如Power View、資料驅動訂閱和Web Farm架構
+          - 商業智慧版（Business Intelligence Edition）
+
+    Sample output:
+    1. 第03章新商業智慧平台安裝與設定
+      1.1 安裝SSRS 2012的前置需求
+        1.1.1 版本限制
+          1.1.1.1 標準版（Standard Edition）
+            1.1.1.1.1 提供報表設計、管理和部署功能
+            1.1.1.1.2 不支援進階功能如Power View、資料驅動訂閱和Web Farm架構
+          1.1.1.2 商業智慧版（Business Intelligence Edition）
+    """
+    lines = content.split(linesep)
+
+    # region Step 1: Find number of spaces used for indentation
+    indents = [len(line) - len(line.lstrip()) for line in lines if line.strip()]
+    min_indent = min(indents)
+    other_indents = [indent for indent in indents if indent > min_indent]
+
+    if not other_indents:  # the same indent for all or it's a single line
+        return min_indent
+
+    indent_unit = min(other_indents) - min_indent
+    # endregion
+
+    def process_line(line, counters, indent_unit):
+        num_spaces = len(line) - len(line.lstrip())
+        indent_level = num_spaces // indent_unit
+
+        if indent_level >= len(counters):
+            counters.extend([0] * (indent_level - len(counters) + 1))
+        else:
+            counters = counters[: indent_level + 1]
+
+        counters[-1] += 1
+        numbering = ".".join(map(str, counters))
+
+        # if there is only one number, add a dot at the end
+        if numbering.count(".") == 0:
+            numbering += "."
+
+        new_line = re.sub(r"^(\s*)\S+\s+", r"\1", line)  # del any list marker
+        new_line = f"{' ' * num_spaces}{numbering} {new_line.lstrip()}"  # add marker
+
+        return new_line, counters
+
+    counters = []
+    new_lines = []
+
+    for line in lines:
+        new_line, counters = process_line(line, counters, indent_unit)
+        new_lines.append(new_line)
+
+    return linesep.join(new_lines)
+
+
 
 def aggregate_summary(results):
     aggs=[]
+    raw_lines = []
     for result in results:
         if isinstance(result,dict):
-            items=[line for line in result['content'].split('\n') if is_numbered_list_member(line)]
+            lines = result['content'].split(os.linesep)
+            items = [line for line in lines if is_numbered_list_member(line)]
             if all([item[:4]=="    " for item in items]):
                 items=[item[4:]for item in items]
             aggs.extend(items)
+            raw_lines.extend(lines)
         elif isinstance(result,str):
             if len(aggs) == 0:
                 aggs.append(result.split('\n')[0])
             aggs.extend([c for c in result.split('\n') if c.startswith('-')])
+    # plan b: generate num list from raw lines when it's not num list
+    if len(aggs) == 0:
+        # print(os.linesep.join(raw_lines)) # for debug
+        # print("=" * 80) # for debug
+        aggs = convert_bullet_to_number_list(raw_lines)
+        # print(os.linesep.join(aggs)) # for debug
+    aggs = os.linesep.join(aggs) # convert to a string for display
     return aggs
 def split_summary(summary_list,max_tokens):
     total_tokens=builtins.sum([estimate_used_tokens(w) + 1 for w in summary_list])
