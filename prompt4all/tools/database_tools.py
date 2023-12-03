089c9cf9fc01e3deced36a602f4145a85e1894b3
Allan <allan@datadecision.ai>
2023-12-03 08:39:39 +0800

    change: T-SQL generation is change to use "gpt-3.5-turbo" for faster speed

--- prompt4all/tools/database_tools.py
+++ prompt4all/tools/database_tools.py
@@ -7,22 +7,24 @@ from prompt4all.utils import regex_utils
 import pandas as pd
 import struct
 from openai import OpenAI
+
 client = OpenAI()
-cxt=context._context()
-engine =create_engine(cxt.conn_string)
+client._custom_headers['Accept-Language'] = 'zh-TW'
+cxt = context._context()
+engine = create_engine(cxt.conn_string)
 
-def query_sql(query_intent:str):
 
-    query_prompt='#zh-TW 基於以下資料結構，請為我撰寫可以回答"{0}"的sql語法，在語法中用來排序、篩選所用到的量值或欄位，或是計算某個量值所用到的分子與分母，請在你的SQL語法中盡量保留，盡量避免CTE，請善用子查詢以及WHERE條件式來篩選案例以提升計算效率，注意別犯除以零的錯誤以及別在排序時重複引用同個欄位，直接輸出，無須解釋。\n"""\n{1}\n"""\n'.format(query_intent,cxt.databse_schema)
-    is_success=False
+def query_sql(query_intent: str):
+    query_prompt = '#zh-TW 你是一個有多年經驗、熟悉T-SQL語法的數據科學家，基於以下資料結構，請為我撰寫可以回答"{0}"的sql語法，在語法中用來排序、篩選所用到的量值或欄位，或是計算某個量值所用到的分子與分母，請在你的SQL語法中盡量保留，盡量避免CTE，請善用子查詢以及WHERE條件式來篩選案例以提升計算效率，注意別犯除以零的錯誤以及別在排序時重複引用同個欄位，直接輸出，無須解釋。\n"""\n{1}\n"""\n'.format(
+        query_intent, cxt.databse_schema)
+    is_success = False
     this_query_prompt = query_prompt
-    retry=0
-    while not is_success and retry<3:
-
-        response=client.chat.completions.create(
-            model="gpt-4-1106-preview",
+    retry = 0
+    while not is_success and retry < 3:
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
             messages=[
-            {'role': 'user', 'content':this_query_prompt}
+                {'role': 'user', 'content': this_query_prompt}
             ],
             temperature=0.3,
             n=1,
@@ -31,44 +33,41 @@ def query_sql(query_intent:str):
         )
         response_message = response.choices[0].message
         try:
-            tsql=regex_utils.extract_code(response_message.content)
+            tsql = regex_utils.extract_code(response_message.content)
             if tsql:
                 with engine.begin() as conn:
                     df = pd.DataFrame(conn.execute(sqlalchemy.text(tsql).execution_options(autocommit=True)))
-                is_success=True
-                save_query_cache(query_intent,tsql)
-                return r'"""\n#資料庫查詢答案\n\n##使用的t-sql\n\n{0}\n\n##查詢獲得的數據\n\n{1}\n\n"""'.format(tsql,df.to_string(index=False))
+                is_success = True
+                save_query_cache(query_intent, tsql)
+                return r'"""\n#資料庫查詢相關內容(請根據查詢結果回答)  \n\n##查詢使用的t-sql  \n\n{0}  \n\n##查詢結果  \n\n  {1}\n\n  """'.format(
+                    tsql, df.to_string(index=False))
             else:
                 raise RuntimeError('Get No SQL')
         except Exception as e:
             print(e)
-            retry+=1
-            this_query_prompt=query_prompt+'\n'+str(e)
+            retry += 1
+            this_query_prompt = query_prompt + '\n' + str(e)
 
 
-def save_query_cache(query_intent,  generated_tsql):
+def save_query_cache(query_intent, generated_tsql):
     def get_embedding(text):
         text = text.replace("\n", " ")
-        response=client.embeddings.create(
+        response = client.embeddings.create(
             model="text-embedding-ada-002",
             input=text,
             encoding_format="float"
         )
         return response.data[0].embedding
+
     try:
         embedding = get_embedding(query_intent)
-        embedding_string =','.join([str(num) for num in embedding])
+        embedding_string = ','.join([str(num) for num in embedding])
         with engine.begin() as conn:
-            query = sqlalchemy.text("INSERT INTO SqlRAG.dbo.QueryIntentCache(QueryIntent, VectorizedQueryIntent, GeneratedTSQL) VALUES (:query_intent, :embedding, :generated_tsql)").execution_options(autocommit=True)
-            result = conn.execute(query, {"query_intent": query_intent, "embedding": embedding_string , "generated_tsql": generated_tsql})
+            query = sqlalchemy.text(
+                "INSERT INTO SqlRAG.dbo.QueryIntentCache(QueryIntent, VectorizedQueryIntent, GeneratedTSQL) VALUES (:query_intent, :embedding, :generated_tsql)").execution_options(
+                autocommit=True)
+            result = conn.execute(query, {"query_intent": query_intent, "embedding": embedding_string,
+                                          "generated_tsql": generated_tsql})
             print(query.text)
     except Exception as e:
         print(e)
-
-
-
-
-
-
-
-
